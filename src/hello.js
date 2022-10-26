04c5c08c7438d9770e22f3d6f995f833c2b22e51
wer340 <hsee1372@gmail.com>
2022-10-26 10:59:07 +0330

    props with class

--- src/hello.js
+++ src/hello.js
@@ -2,7 +2,7 @@ import React ,{Component} from "react";
 
 class Hello extends Component{
     render(){
-        return <h1 class="f-headline  tc">hello this is test</h1>
+        return (<div><h1 className= "f-headline  tc">hello this is test</h1><h4 className= "tc f1">{this.props.greething} ❤</h4> </div>)
     }
 }
 export default Hello;
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greething={"hello"+ "scarlett"} />
+    <Hello greething={"hello "+ " scarlett"} />
   </React.StrictMode>
 );
 
