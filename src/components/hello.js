7688a298e13ac54df60b5e44ce2cd842b4901980
wer340 <hsee1372@gmail.com>
2022-11-17 16:23:57 +0330

    Updates

--- src/components/hello.js
+++
@@ -1,8 +0,0 @@
-import React ,{Component} from "react";
-
-const Hello =(props)=>{
-    
-        return (<div><h1 className= "f-headline  tc">hello this is test</h1><h4 className= "tc f1">{props.greething} ❤</h4> </div>)
-    
-}
-export default Hello;
